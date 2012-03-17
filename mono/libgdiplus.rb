5beb7e932bc1bb0ea2ab553747daa99cc1a470a0
Adam Vandenberg <flangy@gmail.com>
2012-03-17 12:21:45 -0700

    libgdiplus: style nits

--- mono/libgdiplus.rb
+++ mono/libgdiplus.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Libgdiplus < Formula
-  url 'http://ftp.novell.com/pub/mono/sources/libgdiplus/libgdiplus-2.10.tar.bz2'
   homepage 'http://www.mono-project.com/Libgdiplus'
+  url 'http://ftp.novell.com/pub/mono/sources/libgdiplus/libgdiplus-2.10.tar.bz2'
   md5 '451966e8f637e3a1f02d1d30f900255d'
 
   depends_on 'gettext'
@@ -10,17 +10,19 @@ class Libgdiplus < Formula
   depends_on 'libexif'
   depends_on 'glib'
 
+  # Fix compilation against libpng1.5. See:
+  # https://bugs.gentoo.org/355113
+  # https://bugzilla.novell.com/show_bug.cgi?id=666583
   def patches
-    # fixes compilation against libpng1.5
-    # see https://bugs.gentoo.org/355113
-    # and https://bugzilla.novell.com/show_bug.cgi?id=666583
-    { :p0 => 'http://cvsweb.se.netbsd.org/cgi-bin/bsdweb.cgi/~checkout~/pkgsrc/graphics/libgdiplus/patches/patch-aa?rev=1.9;content-type=text%2Fplain' }
+    { :p0 =>
+    'http://cvsweb.se.netbsd.org/cgi-bin/bsdweb.cgi/~checkout~/pkgsrc/graphics/libgdiplus/patches/patch-aa?rev=1.9;content-type=text%2Fplain'
+    }
   end if MacOS.lion?
 
   def install
     ENV.x11
-
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
