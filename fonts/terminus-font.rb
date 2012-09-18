cf33466458864484105a91b371b5288c75e089ca
Justin de Vesine <justin@crowdfavorite.com>
2012-09-18 15:18:46 -0600

    terminus-font depends on X11
    fixes #234

--- fonts/terminus-font.rb
+++ fonts/terminus-font.rb
@@ -5,6 +5,8 @@ class TerminusFont < Formula
   url 'http://sourceforge.net/projects/terminus-font/files/terminus-font-4.36/terminus-font-4.36.tar.gz'
   sha256 '7b0dcebd8ef2e36aeeb7fcd97082ad881e95b2871e40c9a6255377bea6bcd345'
 
+  depends_on :x11
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--x11dir=#{share}/fonts"
